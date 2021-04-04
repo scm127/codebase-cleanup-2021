1b5893afb522a941c3b6e2b8b8cc7f1f4ec16ef1
scm127 <67708429+scm127@users.noreply.github.com>
2021-04-04 19:11:43 -0400

    Update game.py

--- app/game.py
+++ app/game.py
@@ -1,10 +1,17 @@
 
 from random import choice
 
-#
+VALID_OPTIONS=["rock","paper", "scissors"]
+
+def determine_winner(p1,p2):
+    return 
+
+if __name__ == '__main__':
+    pass
+
+
 # USER SELECTION
 #
-VALID_OPTIONS=["rock","paper", "scissors"]
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
 if u not in VALID_OPTIONS:
@@ -42,6 +49,25 @@ print("COMPUTER CHOICE:", c)
 #    print("The user wins")
 #elif u == "scissors" and c == "scissors":
 #    print("It's a tie!")
-#if u=="rock":
-#    if c=="rock":
-#        print
+if u=="rock":
+    if c=="rock":
+        print("It's a tie!")
+    elif c=="paper":
+        print("The computer wins")
+    if c=="scissors":
+        print("The user wins")
+elif u=="paper":
+    if c=="rock":
+        print("The user wins")
+    elif c=="paper":
+        print("It's a tie!")
+    elif c=="scissors":
+        print("The computer wins")
+elif u=="scissors":
+    if c=="rock":
+        print("The computer wins")
+    elif c=="paper":
+        print("The user wins")
+    elif c=="scissors":
+        print("It's a tie!")
+
