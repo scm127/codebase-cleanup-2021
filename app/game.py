5be9d75314a503b013ac5e3ffe4090f12d1ce189
scm127 <67708429+scm127@users.noreply.github.com>
2021-04-04 22:07:11 -0400

    Created a determine winner function (#5)

--- app/game.py
+++ app/game.py
@@ -3,71 +3,69 @@ from random import choice
 
 VALID_OPTIONS=["rock","paper", "scissors"]
 
-def determine_winner(p1,p2):
-    return 
+def determine_winner(choice1,choice2):
+    """
+    Params:
+    Choice1 and Choice2 are one of the strings: "rock", "paper", or "scissors"
+    """
+    winners = {
+        "rock":{
+            "rock":None,
+            "paper":"paper",
+            "scissors":"rock",
+        },
+        "paper":{
+            "rock":"paper",
+            "paper":None,
+            "scissors":"scissors",
+        },
+        "scissors":{
+            "rock":"rock",
+            "paper":"scissors",
+            "scissors":None,
+        },
+    } 
+    winning_choice=winners[choice1][choice2]
+    return winning_choice
+
 
 if __name__ == '__main__':
-    pass
 
 
-# USER SELECTION
-#
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in VALID_OPTIONS:
-    print("OOPS, TRY AGAIN")
-    exit()
 
-#
-# COMPUTER SELECTION
-#
 
-c = choice(VALID_OPTIONS)
-print("COMPUTER CHOICE:", c)
 
-#
-# DETERMINATION OF WINNER
-#
 
-#if u == "rock" and c == "rock":
-#    print("It's a tie!")
-#elif u == "rock" and c == "paper":
-#    print("The computer wins")
-#elif u == "rock" and c == "scissors":
-#    print("The user wins")
+        
 
-#elif u == "paper" and c == "rock":
-#    print("The computer wins")
-#elif u == "paper" and c == "paper":
-#    print("It's a tie!")
-#elif u == "paper" and c == "scissors":
-#    print("The user wins")
 
-#elif u == "scissors" and c == "rock":
-#    print("The computer wins")
-#elif u == "scissors" and c == "paper":
-#    print("The user wins")
-#elif u == "scissors" and c == "scissors":
-#    print("It's a tie!")
-if u=="rock":
-    if c=="rock":
-        print("It's a tie!")
-    elif c=="paper":
-        print("The computer wins")
-    if c=="scissors":
-        print("The user wins")
-elif u=="paper":
-    if c=="rock":
-        print("The user wins")
-    elif c=="paper":
-        print("It's a tie!")
-    elif c=="scissors":
-        print("The computer wins")
-elif u=="scissors":
-    if c=="rock":
-        print("The computer wins")
-    elif c=="paper":
-        print("The user wins")
-    elif c=="scissors":
-        print("It's a tie!")
+    # USER SELECTION
+    #
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in VALID_OPTIONS:
+        print("OOPS, TRY AGAIN")
+        exit()
+
+    #
+    # COMPUTER SELECTION
+    #
+
+    c = choice(VALID_OPTIONS)
+    print("COMPUTER CHOICE:", c)
+
+    #
+    # DETERMINATION OF WINNER
+    #
+
+ 
+    winner=determine_winner(u, c)
+    if winner==u:
+        print("You won!")
+    elif winner==c:
+        print("Computer Won!")
+    elif winner==None:
+        print("Tie!")
+
+
 
